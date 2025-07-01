8fcba8d043d1e3ae480b3b1c2173ee8cd4dcb108
A0A8 <155348609+A0A8@users.noreply.github.com>
2025-07-01 18:40:44 +0800

    Update euserv_auto_renew.py

--- euserv_auto_renew.py
+++ euserv_auto_renew.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/95.0.4638.69 Safari/537.36"
+)
 import os
 import re
 import time

2c0ebaab709bc875c63522a1665706f6df5069ca
A0A8 <155348609+A0A8@users.noreply.github.com>
2025-07-01 15:06:34 +0800

    Update euserv_auto_renew.py

--- euserv_auto_renew.py
+++ euserv_auto_renew.py
@@ -162,9 +162,7 @@ def check(sid, session):
         log("[Check] 所有续期成功")
 
 def telegram():
-    data = {"chat_id": TG_USER_ID, "text": "EUserv续费日志
-
-"+desp}
+    data = {"chat_id": TG_USER_ID, "text": "EUserv续费日志"+desp}
     requests.post(f"{TG_API_HOST}/bot{TG_BOT_TOKEN}/sendMessage", data=data)
 
 def send_mail():
